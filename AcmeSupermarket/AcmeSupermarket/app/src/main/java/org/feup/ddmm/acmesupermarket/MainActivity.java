61f945cbcfc28053cb85f353b764f2b398925ac6
Diogo Dores <diogo.dores@gmail.com>
2019-10-30 16:17:52 +0000

    Added Main Page frontend

--- AcmeSupermarket/AcmeSupermarket/app/src/main/java/org/feup/ddmm/acmesupermarket/MainActivity.java
+++ AcmeSupermarket/AcmeSupermarket/app/src/main/java/org/feup/ddmm/acmesupermarket/MainActivity.java
@@ -3,8 +3,11 @@ package org.feup.ddmm.acmesupermarket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button login, register;
