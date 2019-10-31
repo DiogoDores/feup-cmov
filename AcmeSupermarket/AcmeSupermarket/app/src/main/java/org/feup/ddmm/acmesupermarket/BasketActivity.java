12f195c66e3db6ec3cbbb73a993927669f2a4e1d
Diogo Dores <diogo.dores@gmail.com>
2019-10-31 14:54:51 +0000

    Started working on Basket Activity

---
+++ AcmeSupermarket/AcmeSupermarket/app/src/main/java/org/feup/ddmm/acmesupermarket/BasketActivity.java
@@ -0,0 +1,13 @@
+package org.feup.ddmm.acmesupermarket;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BasketActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
--- AcmeSupermarket/AcmeSupermarket/app/src/main/java/org/feup/ddmm/acmesupermarket/MainActivity.java
+++ AcmeSupermarket/AcmeSupermarket/app/src/main/java/org/feup/ddmm/acmesupermarket/MainActivity.java
@@ -3,15 +3,10 @@ package org.feup.ddmm.acmesupermarket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Typeface;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button login, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
