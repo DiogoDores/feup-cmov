9113168845076443d006caafc8f70527f6311209
Diogo Dores <diogo.dores@gmail.com>
2019-11-01 23:02:39 +0000

    sou ganda burro (e também fiz mais frontend)

--- AcmeSupermarket/AcmeSupermarket/app/src/main/java/org/feup/ddmm/acmesupermarket/BasketActivity.java
+++ AcmeSupermarket/AcmeSupermarket/app/src/main/java/org/feup/ddmm/acmesupermarket/BasketActivity.java
@@ -7,7 +7,12 @@ public class BasketActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_basket);
     }
 
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
 }
