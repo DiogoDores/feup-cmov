712e04c444079308e8981fb35195eb13aba2a117
Diogo Dores <diogo.dores@gmail.com>
2019-11-11 11:40:02 +0000

    Added receipts page and continued basket

---
+++ AcmeSupermarket/AcmeSupermarket/app/src/main/java/org/feup/ddmm/acmesupermarket/ReceiptsActivity.java
@@ -0,0 +1,14 @@
+package org.feup.ddmm.acmesupermarket;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReceiptsActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipts);
+    }
+
+}
