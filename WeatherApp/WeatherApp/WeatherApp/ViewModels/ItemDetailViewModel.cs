685ce5d76a7838c9b54e276874aecc07cd045171
Diogo Dores <diogo.dores@gmail.com>
2019-11-28 16:03:57 +0000

    Added xamarin project

---
+++ WeatherApp/WeatherApp/WeatherApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using WeatherApp.Models;
+
+namespace WeatherApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
