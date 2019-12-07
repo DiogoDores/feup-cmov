ef19efd0c81cbeadd3183b7b53b1288a3640329e
aquelemiguel <miguelmanofernandes@gmail.com>
2019-12-07 20:57:52 +0000

    Add GET call support

---
+++ MyWeather/MyWeather/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace MyWeather
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            string apiResult = "";
+            string url = "https://jsonplaceholder.typicode.com/posts/1";
+
+            Task task = new Task(() => { apiResult = AccessWebAsync(url).Result; });
+            task.Start();
+            task.Wait();
+            System.Diagnostics.Debug.WriteLine(apiResult);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        async Task<string> AccessWebAsync(string url)
+        {
+            HttpClient client = new HttpClient();
+            Task<string> getStringTask = client.GetStringAsync(url);
+            return await getStringTask;
+        }
+    }
+}
