a2ce04ee301c426a606aaea097291534c6c5b3a5
Diogo Dores <diogo.dores@gmail.com>
2019-12-14 22:39:36 +0000

    Added temporary carousel

--- OurWeather/OurWeather/MainPage.xaml.cs
+++ OurWeather/OurWeather/MainPage.xaml.cs
@@ -17,21 +17,62 @@ namespace OurWeather
     public partial class MainPage : ContentPage
     {
         private readonly FavouritesPage favouritesPage;
-
+        private readonly List<Color> _backgroundColors = new List<Color>();
         public MainPage()
         {
+
             InitializeComponent();
+
+            var model = new CarouselViewModel
+            {
+                Items = new List<CarouselItem>()
+                {
+                    // Just create some dummy data here for now.
+                    new CarouselItem{ Type="JUICY AND ORANGE", ImageSrc="logo.png", Name = "Porto", Price = 120, Title = "22º", BackgroundColor= Color.FromHex("#000000"), StartColor=Color.FromHex("#f3463f"),  EndColor=Color.FromHex("#fece49")},
+                    new CarouselItem{ Type="NOT A TYPICAL FRUIT", ImageSrc="tomato.png", Name = "TERRIBLE TOMATO", Price = 129, Title = "TERRIBLE TOMATO", BackgroundColor= Color.FromHex("#fab62a"), StartColor=Color.FromHex("#42a7ff"),  EndColor=Color.FromHex("#fab62a")},
+                    new CarouselItem{ Type="SWEET AND GREEN", ImageSrc="pear.png", Name = "PEAR PARTY", Price = 140, Title = "PEAR PARTY", BackgroundColor= Color.FromHex("#425cfc"), StartColor=Color.FromHex("#33ccf3"),  EndColor=Color.FromHex("#ccee44")}
+                }
+            };
+
+            BindingContext = model;
+
+            // Create out a list of background colors based on our items colors so we can do a gradient on scroll.
+            for (int i = 0; i < model.Items.Count; i++)
+            {
+                var current = model.Items[i];
+                var next = model.Items.Count > i + 1 ? model.Items[i + 1] : null;
+
+                if (next != null)
+                    _backgroundColors.AddRange(SetGradients(current.BackgroundColor, next.BackgroundColor, 375));
+                else
+                    _backgroundColors.Add(current.BackgroundColor);
+            }
+
             this.favouritesPage = new FavouritesPage();
 
             // Serialize the district JSON and send it to the favourites page.
             this.favouritesPage.SetDistricts(this.DictionarySerializeDistrictJSON());
         }
 
-        private CarouselViewModel PopulateCarousel(List<ForecastHour> districts)
+        /*private CarouselViewModel PopulateCarousel(List<ForecastHour> districts)
         {
             List<CarouselItem> items = new List<CarouselItem>();
             districts.ForEach(dist => items.Add(new CarouselItem { Name = dist.name }));
             return new CarouselViewModel { Items = items };
+        }*/
+
+        public void Handle_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+        {
+            var position = e.HorizontalOffset;
+
+            // Set the background color of our page to the item in the color gradient
+            // array, matching the current scrollindex.
+            if (position > _backgroundColors.Count - 1)
+                page.BackgroundColor = _backgroundColors.Last();
+            else if (position < 0)
+                page.BackgroundColor = _backgroundColors.First();
+            else
+                page.BackgroundColor = _backgroundColors[(int)position];
         }
 
         private List<DistrictInfo> DictionarySerializeDistrictJSON()
@@ -57,16 +98,39 @@ namespace OurWeather
             });
 
             // Populate carousel and bind it to context.
-            CarouselViewModel model = this.PopulateCarousel(districts);
-            BindingContext = model;
+            //CarouselViewModel model = this.PopulateCarousel(districts);
+            //BindingContext = model;
 
-            base.OnAppearing();
+            //base.OnAppearing();
         }
 
 
         private async void OnButtonClicked(object sender, EventArgs args)
         {
             await Navigation.PushAsync(favouritesPage);
+        }
+
+        // Create a list of all the colors in between our start and end color.
+        public static IEnumerable<Color> SetGradients(Color start, Color end, int steps)
+        {
+            var colorList = new List<Color>();
+
+            double aStep = ((end.A * 255) - (start.A * 255)) / steps;
+            double rStep = ((end.R * 255) - (start.R * 255)) / steps;
+            double gStep = ((end.G * 255) - (start.G * 255)) / steps;
+            double bStep = ((end.B * 255) - (start.B * 255)) / steps;
+
+            for (int i = 0; i < steps; i++)
+            {
+                var a = (start.A * 255) + (int)(aStep * i);
+                var r = (start.R * 255) + (int)(rStep * i);
+                var g = (start.G * 255) + (int)(gStep * i);
+                var b = (start.B * 255) + (int)(bStep * i);
+
+                colorList.Add(Color.FromRgba(r / 255, g / 255, b / 255, a / 255));
+            }
+
+            return colorList;
         }
     }
 }
--- OurWeather/OurWeather/Models/CarouselItem.cs
+++ OurWeather/OurWeather/Models/CarouselItem.cs
@@ -1,14 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Text;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 
 namespace OurWeather.Models
 {
     public class CarouselItem : INotifyPropertyChanged
     {
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public Color StartColor { get; set; }
+        public Color EndColor { get; set; }
+        public Color BackgroundColor { get; set; }
+        public string Type { get; set; }
+        public string ImageSrc { get; set; }
+        public string Description { get; set; } = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pretium dolor sed felis varius, at elementum nunc blandit.";
+
+        private double _position;
+        private double _scale;
+
+        public CarouselItem()
+        {
+            Scale = 1;
+        }
+
+        public double Position
+        {
+            get { return _position; }
+            set
+            {
+                _position = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Scale
+        {
+            get { return _scale; }
+            set
+            {
+                _scale = value;
+                OnPropertyChanged();
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public string Name { get; set; }
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
